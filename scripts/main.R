26bf679733b7100e923e71ae8333ab2661f36a78
evandro@stk9 <evandroluisalves13@gmail.com>
2022-12-14 20:48:48 -0300

    Changes

--- scripts/main.R
+++ scripts/main.R
@@ -1,9 +1,23 @@
 
-
 #
-usethis::create_github_token()
+# usethis::create_github_token()
 
 # Token: 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
 
 #
 usethis::edit_r_environ()
+
+
+# GITHUB_PAT= "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"
+
+#
+usethis::use_git_remote("origin", url = NULL, overwrite = TRUE)
+
+#
+usethis::use_github(protocol = "https", private = TRUE
+                    # auth_token = Sys.getenv("GITHUB_PAT")
+                    )
+
+#
+usethis::use_mit_license()
+
